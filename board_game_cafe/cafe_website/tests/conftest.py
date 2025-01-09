d1c736c61538d49db2a449a406a7e679a0094a15
Corbo <ppyrgiel@wp.pl>
2025-01-09 12:14:37 +0100

    additional tests

--- board_game_cafe/cafe_website/tests/conftest.py
+++ board_game_cafe/cafe_website/tests/conftest.py
@@ -10,3 +10,23 @@ def user(db):
 def game(db):
     from cafe_website.models import Game
     return Game.objects.create(name="Wingspan", description="bird enthusiasts")
+
+@pytest.fixture
+def table(db):
+    from cafe_website.models import Table
+    return Table.objects.create(number=1, capacity=4)
+
+# @pytest.fixture
+# def reservation(db, user):
+#     from cafe_website.models import Reservation, Game, Table
+#
+#     game = Game.objects.create(name="Wingspan", description="bird enthusiasts")
+#     table = Table.objects.create(number=1, capacity=4)
+#
+#     return Reservation.objects.create(
+#         user=user,
+#         game=game,
+#         table=table,
+#         start_time="2024-12-02 11:11:00+00:00",
+#         end_time="2024-12-04 11:11:00+00:00"
+#     )
--- board_game_cafe/cafe_website/tests/tests.py
+++ board_game_cafe/cafe_website/tests/tests.py
@@ -9,14 +9,12 @@ def test_game_collection_view(client):
     assert response.status_code == 200
     assert "Our collection" in response.content.decode()
 
-
 @pytest.mark.django_db
 def test_game_details_view(client, game):
     response = client.get(reverse('game_details', args=[game.pk]))
     assert response.status_code == 200
     assert "Wingspan" in response.content.decode()
     assert "bird enthusiasts" in response.content.decode()
-
 
 @pytest.mark.django_db
 def test_user_login_view(client, user):
@@ -60,3 +58,59 @@ def test_user_register_view(client):
 
     User = get_user_model()
     assert User.objects.filter(username="testuser").exists()
+
+@pytest.mark.django_db
+def test_user_register_invalid_credentials(client):
+    url = reverse('register')
+    register_data = {
+        "username": "testuser",
+        "email": "testemail@mail.com",
+        "password1": "123",
+        "password2": "123",
+    }
+    response = client.post(url, register_data)
+
+    assert response.status_code == 200
+
+    User = get_user_model()
+    assert not User.objects.filter(username="testuser").exists()
+
+@pytest.mark.django_db
+def test_user_logout_view(client, user):
+    client.login(username="testuser", password="password123")
+    response = client.post(reverse('logout'))
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+@pytest.mark.django_db
+def test_reservation_create_view(client, user, game, table):
+    client.login(username="testuser", password="password123")
+    url = reverse('reservation', kwargs={'game_id': game.id})
+    reservation_data = {
+        "game": game.id,
+        "table": table.id,
+        "start_time": "2025-12-01 11:11:00+00:00",
+        "end_time": "2025-12-02 11:11:00+00:00"
+    }
+    response = client.post(url, reservation_data)
+
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+    from cafe_website.models import Reservation
+    assert Reservation.objects.filter(user=user, game=game, table=table).exists()
+
+@pytest.mark.django_db
+def test_reservation_create_view_invalid_data(client, user, game, table, reservation):
+    client.login(username="testuser", password="password123")
+    url = reverse('reservation', kwargs={'game_id': game.id})
+    reservation_data = {
+        "game": game.id,
+        "table": table.id,
+        "start_time": "2024-12-01 11:11:00+00:00",
+        "end_time": "2024-12-03 11:11:00+00:00"
+    }
+    response = client.post(url, reservation_data)
+
+    assert response.status_code == 200
+    assert response.url == reverse('reservation', kwargs={'game_id': game.id})
